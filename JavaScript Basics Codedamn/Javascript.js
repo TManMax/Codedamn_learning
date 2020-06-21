09a5333416870bad9086953ecf9981be0cd1ab9f
Tola <tolahenry@hotmail.com>
2020-06-22 00:08:15 +0100

    Information update 22_06_2020

---
+++ JavaScript Basics Codedamn/Javascript.js
@@ -0,0 +1,77 @@
+/*
+
+// Information of variables.
+
+var, let, const
+let myName = "Tola"; (Simliar to var but different)
+var iLike = "food";
+const myAge = 24; (Cannot be changed)
+
+
+//Information of data types.
+
+let string =  "This is a normal string";
+let number = 42;
+let bool = false;
+let array = [1,2,3,4,5,"a","b"];
+let _undefined =  undefined;
+let _null = null;
+let _NaN = NaN;
+
+let object = {
+    "keyname": "value"
+}
+
+//Information on functions
+
+function myAwesomeFunction() {
+    //all of your code
+}
+
+myAwesomeFunction() (This calls the function)
+
+//Function Parameters information
+(How to make function parameters)
+function myAwesomeFunction(myString, myString2 = 0) {
+    //all of your code
+    console.log(myString);
+    console.log(myString);
+}
+
+myAwesomeFunction("AJJndk31","one");
+myAwesomeFunction("This is my function");
+
+
+//Example of array.filter.
+let array = [1,2,3,4,5,6]
+
+let newArray = array.filter(element => {
+    if(element > 2) return true;
+    return false
+})
+
+console.log(newArray);
+
+
+//Example of array.map (Map gives you a new array based on the old array which you can pass element by element)
+let array1 = [1,2,3,4,5,6,7]
+
+let array2 = array1.map((element) => {
+    return element * 2;
+})
+
+console.log(array2);
+
+
+//Array.forEach information (Just like .map but instead just doing something with the element)
+
+let array = ["Tola", "Demetrius", "Henry"]
+
+array.forEach((element) => {
+    console.log(element);
+})
+
+*/
+
+//Array reduce information
+
