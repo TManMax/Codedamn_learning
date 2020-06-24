99af8d8a485b6ff90c68237bcee7eb8681b36fd1
Tola Henry <tolahenry@hotmail.com>
2020-06-24 12:54:30 +0100

    Laptop work update 24_06_2020

---
+++ JavaScript Basics Codedamn/Javascript2.js
@@ -0,0 +1,9 @@
+let test1 = "This is just a test";
+var test2 = 0;
+var test3 = 0;
+var test4 = 24;
+
+if(test2 < test4) {
+    test2++
+    console.log(`the variable of test of test2 is now ${test2}`);
+}
