2e25176e11ee2ca99bb5265ea220e6891481fb58
Tola <tolahenry@hotmail.com>
2020-06-24 20:31:14 +0100

    update work dekstop PC 24_06_2020

--- JavaScript Basics Codedamn/Javascript2.js
+++ JavaScript Basics Codedamn/Javascript2.js
@@ -1,9 +1,27 @@
-let test1 = "This is just a test";
-var test2 = 0;
-var test3 = 0;
-var test4 = 24;
-
-if(test2 < test4) {
-    test2++
-    console.log(`the variable of test of test2 is now ${test2}`);
+function shoppingList(basket1,basket2,basket3,basket4) {
+    var basket1 = "tomato, pasta, mushrooms"
+    var basket2 = "milk, pear, drink, water"
+    var basket3 = "dried onion, oranges, cake"
+    var basket4 = "tomato ketchup, orange juice, sweet and sour sauce"
+    console.log(`My current shopping list includes ${basket1}`)
+    console.log(`My second shopping list includes ${basket2}`)
+    console.log(`My third shopping list is for later but for now it includes ${basket3}`)
+    console.log(`My fourth and final shopping list for the month includes ${basket4}`)
 }
+    
+function addItems(item1,item2,item3) {
+var item1 = "cous cous"
+var item2 = "gravy cubs"
+var item3 = "tea cakes"
+
+if (item1 == "cous cous") {
+    updateBasket = basket1 + item1
+    console.log("Done")
+}
+
+else {
+    console.log("Not done")
+};
+};
+
+addItems()
